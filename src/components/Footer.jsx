e8e6674cda1a2410beced7027c6dc65b301c649a
Smarteq-Onur <onurkordogan@gmail.com>
2025-02-16 22:15:21 +0300

    update footer copyright year from 2022 to 2025

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className='flex flex-col md:flex-row h-16 bg-white  md:justify-between items-center justify-center text-lightkozy font-medium px-4 text-sm  py-4 '>
-     <div>2022© turna</div>
+     <div>2025© quatro</div>
      <div className='flex gap-4 '>
         <Link to="#">About</Link>
         <Link to="#">Support</Link>
