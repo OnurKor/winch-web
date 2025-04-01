1a20107af2d085ec0914b3fdbce25944bf8a1bcb
Smarteq-Onur <onurkordogan@gmail.com>
2025-04-01 09:15:02 +0300

    fix: improve formatting and spacing in AppHeader component for consistency

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const AppHeader = ({header,subHeader,buttonName,func,buttonColor}) => {
+const AppHeader = ({ header, subHeader, buttonName, func, buttonColor }) => {
   return (
-    <div className='flex justify-between items-center mb-4'>
-      <div className=''>
-        <div className='text-md font-bold text-default'>{header}</div>
-        <div className='text-sm text-lightkozy'>{subHeader}</div>
+    <div className="flex justify-between items-center mb-4">
+      <div className="">
+        <div className="text-md font-bold text-default">{header}</div>
+        <div className="text-sm text-lightkozy">{subHeader}</div>
       </div>
       {buttonName && (
-  <button
-    onClick={func}
-    className={`${buttonColor ? `bg-red-500` : "bg-lightkozy"} p-2 text-white rounded-lg text-[12px] `}
-  >
-    {buttonName}
-  </button>
-)}
- </div>
-  )
-}
+        <button
+          onClick={func}
+          className={`${
+            buttonColor ? `bg-red-500` : "bg-lightkozy"
+          } p-2 text-white rounded-lg text-[12px] `}
+        >
+          {buttonName}
+        </button>
+      )}
+    </div>
+  );
+};
 
-export default AppHeader
+export default AppHeader;
