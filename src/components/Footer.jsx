7d77f24ddb11dea6f76443e655bea0c379c469d0
Smarteq-Onur <onurkordogan@gmail.com>
2025-04-02 23:31:39 +0300

    fix: standardize formatting and spacing in Footer component for consistency

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className='flex flex-col md:flex-row h-16 bg-white  md:justify-between items-center justify-center text-lightkozy font-medium px-4 text-sm  py-4 '>
-     <div>2025© quatro</div>
-     <div className='flex gap-4 '>
-        <Link to="https://quatrowinch.com/">Quatro Winch | Her Duruma Hazırlıklı Ol</Link>
-     </div>
+    <div className="flex flex-col md:flex-row h-16 bg-white  md:justify-between items-center justify-center text-lightkozy font-medium px-4 text-sm  py-4 ">
+      <div>2025© quatro</div>
+      <div className="flex gap-4 ">
+        <Link to="https://quatrowinch.com/">
+          Quatro Winch | Her Duruma Hazırlıklı Ol
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
