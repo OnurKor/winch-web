269d0b16dad076df1f937c633a0b4cf1a0e8b18a
Smarteq-Onur <onurkordogan@gmail.com>
2025-04-11 23:44:32 +0300

    fix: standardize formatting and spacing in Toast component for consistency

--- src/components/toastMessage/Toast.js
+++ src/components/toastMessage/Toast.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectErrorMessage, setErrorMessage } from "../../store/services/mainSlice";
+import {
+  selectErrorMessage,
+  setErrorMessage,
+} from "../../store/services/mainSlice";
 
 const Toast = () => {
   const dispatch = useDispatch();
   const errorMessage = useSelector(selectErrorMessage);
-  
 
   useEffect(() => {
     if (errorMessage) {
@@ -17,14 +19,13 @@ const Toast = () => {
     }
   }, [errorMessage, dispatch]);
 
-  
   if (!errorMessage) return null;
   return (
     <div className="fixed bottom-12 right-4 bg-white  text-default text-xs font-medium px-4 py-2 rounded shadow-custom z-[900]">
       {/* {errorMessage.replace(/[()]/g, "")} */}
       Bir Hata Oluştu...
-        {/* Progress Bar */}
-        <div className="w-full h-1 bg-lightkozy mt-2 overflow-hidden">
+      {/* Progress Bar */}
+      <div className="w-full h-1 bg-lightkozy mt-2 overflow-hidden">
         <div className="h-full bg-red-500 progress-bar"></div>
       </div>
     </div>
