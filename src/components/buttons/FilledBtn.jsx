c3b6d4f4b1f22d823e873e2f296aa0620a6384cc
Smarteq-Onur <onurkordogan@gmail.com>
2025-05-07 22:55:12 +0300

    fix: standardize formatting and spacing in FilledBtn.jsx for consistency

--- src/components/buttons/FilledBtn.jsx
+++ src/components/buttons/FilledBtn.jsx
@@ -1,11 +1,14 @@
-
 import React from "react";
 
-export default function FilledBtn({ title, func , image,disabled}) {
+export default function FilledBtn({ title, func, image, disabled }) {
   return (
     <button
       onClick={func}
-      className={`${disabled ? "bg-greykozy text-secondary border-kozydarkgrey" : " bg-lightkozy p-2 text-white rounded-lg text-[12px]  "} rounded-[4px] px-6 py-2 ease-in-out duration-300 h-12 border-[1px] flex justify-center items-center gap-3 w-full md:w-fit`}
+      className={`${
+        disabled
+          ? "bg-greykozy text-secondary border-kozydarkgrey"
+          : " bg-lightkozy p-2 text-white rounded-lg text-[12px]  "
+      } rounded-[4px] px-6 py-2 ease-in-out duration-300 h-12 border-[1px] flex justify-center items-center gap-3 w-full md:w-fit`}
     >
       <div>{title}</div>
     </button>
